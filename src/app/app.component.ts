2d47681c28ba3269702ee1ce1aa4046efeb58433
Yonatan <yonatan.ramirez557@gmail.com>
2018-07-05 14:54:02 -0700

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,12 @@ export class AppComponent {
   title = 'app';
   // tslint:disable-next-line:no-inferrable-types
   loadedFeature: string = 'bodyComp';
-  bg: string;
+  // bg: string;
+  bg2: string;
   constructor() {
     // tslint:disable-next-line:max-line-length
-    this.bg = 'https://www.dhresource.com/0x0s/f2-albu-g3-M00-F1-70-rBVaHVSv75-AeppOAAaQTMblXTE043.jpg/vinyl-custom-photography-backdrops-brick.jpg';
+    // this.bg = 'https://www.dhresource.com/0x0s/f2-albu-g3-M00-F1-70-rBVaHVSv75-AeppOAAaQTMblXTE043.jpg/vinyl-custom-photography-backdrops-brick.jpg';
+    this.bg2 = 'http://blog.hostbaby.com/wp-content/uploads/2013/07/scuffedstatic_blue_1920x1234.jpg';
   }
 
   navigate(feature: string) {
